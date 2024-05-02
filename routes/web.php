a7de8756470f90dd37d7cf7f1ccaa9a9bdb334b9
Vihar Ramavat <viharramavat@gmail.com>
2024-05-02 10:56:13 +0530

    starting of the project

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/register', 'AuthController@showRegistrationForm')->name('register');
+Route::post('/register', 'AuthController@register');
+Route::get('/login', 'AuthController@showLoginForm')->name('login');
