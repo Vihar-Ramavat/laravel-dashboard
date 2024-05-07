89902f3751c94620485c978330ab10509a403ca6
Vihar Ramavat <viharramavat@gmail.com>
2024-05-07 10:34:18 +0530

    dashboard-login-copy

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,5 @@
 @section('dashboard_content')
 <div>
     <h1 class="text-2xl font-bold">Welcome to your Dashboard, {{ $user->username }}</h1>
-    <p>Your user ID is: {{ $user->id }}</p>
 </div>
 @endsection
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,11 +1,9 @@
-<!-- resources/views/layouts/dashboard.blade.php -->
-
 @extends('layouts.app')
 
 @section('content')
 <div class="flex">
     <!-- Sidebar -->
-    <div class="w-1/6 bg-gray-900 text-gray-100 h-screen py-4 rounded-r-lg flex flex-col justify-between">
+    <div class="w-1/6 bg-gray-900 text-gray-100 h-screen py-4 pb-0 rounded-r-lg flex flex-col justify-between">
         <div>
             <ul class="text-base">
                 <li class="px-4 py-2 hover:bg-gray-800 rounded">
@@ -27,8 +25,19 @@
                 </li>
             </ul>
         </div>
-        <div class="mt-auto">
-            <a href="#" class="block px-4 py-2 text-sm text-gray-400 text-center">{{ Auth::user()->username }}</a>
+        <div class="user profile bg-gray-800 py-2 hover:bg-gray-700 transition duration-300">
+            <a href="#" class="bg-gray-800 py-2 hover:bg-gray-700 transition duration-300">
+                <div class="flex items-center w-full pl-5">
+                    <div class="rounded-full h-7 w-7 flex items-center justify-center bg-purple-400 text-black text-lg font-bold shadow-md">
+                        {{ substr(Auth::user()->username, 0, 1) }}
+                    </div>
+                    <span class="ml-4 text-sm text-white transition duration-300">{{ Auth::user()->username }}</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 flex">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                      </svg>
+                      
+                </div>
+            </a>
         </div>
     </div>
     <!-- Main Content Area -->
